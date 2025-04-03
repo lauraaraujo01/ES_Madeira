96901fff9383f46fdf25a4648144e71eb2a2b3f5
lauraaraujo01 <lmtao1@iscte-iul.pt>
2025-04-03 16:14:16 +0100

    + updates no test_one.py

--- test_one.py
+++ test_one.py
@@ -3,6 +3,11 @@ class Grafo:
         self.vertices = propiedades
         self.grafo = [[] for i in range(self.vertices)]
 
+    def load_dataset(self, dataset):
+        # Implementar a lógica para carregar o dataset
+        pass
+
+
     def adicionar_aresta(self, u, v,):
         self.grafo[u].append((v))
         self.grafo[v].append((u))
@@ -10,3 +15,13 @@ class Grafo:
     def show_list(self):
         for i in range(self.vertices):
             print(f'{i}: {self.grafo[i]}')
+
+
+if __name__ == '__main__':
+    grafo = Grafo(5)
+    grafo.adicionar_aresta(0, 1)
+    grafo.adicionar_aresta(0, 2)
+    grafo.adicionar_aresta(1, 2)
+    grafo.adicionar_aresta(1, 3)
+    grafo.adicionar_aresta(2, 4)
+    grafo.show_list()
